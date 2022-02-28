17f8efbebdf4d436315d6c8e647f6ba9c49ea978
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-02-28 13:37:59 +0600

    added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const error = document.getElementById('error')
 const subMain = document.getElementById('sub-main')
 const main = document.getElementById('main')
+
+ const spinner = (styleSpinner)=>{
+   const spinner = document.getElementById('spinner').style.display = styleSpinner
+ }
+ 
 const cocktelDetails=()=>{
+     spinner('block')
     const inputField = document.getElementById('input-field')
     const inputValue = inputField.value
      inputField.value=''
@@ -20,22 +26,45 @@ const cocktelDetails=()=>{
       }
 }
  const singelCocktel =(cocktels)=>{
-     for(const cocktel of cocktels){
-          console.log(cocktel)
-         const div = document.createElement('div')
-        //  div.classList.add(col-lg-4)
-         div.innerHTML= `
-         <div class="card mt-4" style="width: 18rem;">
-         <img src="${cocktel.strDrinkThumb}" class="card-img-top" alt="...">
-         <div class="card-body">
-           <h5 class="card-title">${cocktel.strDrink}</h5>
-           <p class="card-text"> ${cocktel.strAlcoholic}.</p>
-           <button onclick ="detailId('${cocktel.idDrink}')" class="btn btn-primary">Go somewhere</button>
+    if(!cocktels){
+       error.innerText ='Sorry items not found'
+    }
+     else{
+       cocktels?.forEach(cocktel => {
+        console.log(cocktel)
+           const div = document.createElement('div')
+           div.innerHTML= `
+           <div class="card mt-4" style="width: 18rem;">
+           <img src="${cocktel.strDrinkThumb}" class="card-img-top" alt="...">
+           <div class="card-body">
+             <h5 class="card-title">${cocktel.strDrink}</h5>
+             <p class="card-text"> ${cocktel.strAlcoholic}.</p>
+             <button onclick ="detailId('${cocktel.idDrink}')" class="btn btn-danger px-4">Details</button>
+           </div>
          </div>
-       </div>
-         `
-         main.appendChild(div)
-     }
+           `
+           main.appendChild(div)
+       
+       });
+      }
+    //   for(const cocktel of  cocktels){
+    //     console.log(cocktel)
+    //    const div = document.createElement('div')
+    //    div.innerHTML= `
+    //    <div class="card mt-4" style="width: 18rem;">
+    //    <img src="${cocktel.strDrinkThumb}" class="card-img-top" alt="...">
+    //    <div class="card-body">
+    //      <h5 class="card-title">${cocktel.strDrink}</h5>
+    //      <p class="card-text"> ${cocktel.strAlcoholic}.</p>
+    //      <button onclick ="detailId('${cocktel.idDrink}')" class="btn btn-danger px-4">Details</button>
+    //    </div>
+    //  </div>
+    //    `
+      //  main.appendChild(div)
+  //  }
+   
+    //  }
+     spinner('none')
  }
   const detailId =(Drink)=>{
     console.log(Drink)
@@ -46,6 +75,7 @@ const cocktelDetails=()=>{
   }
     
   const idDetails=(id)=>{
+    //  spinner('block')
      main.innerHTML =''
   //  console.log(id)
    const div = document.createElement('div')
@@ -59,4 +89,5 @@ const cocktelDetails=()=>{
   </div>
     `
      subMain.appendChild(div)
+      // spinner('none')
   } 
